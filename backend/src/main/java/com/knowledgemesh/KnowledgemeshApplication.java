3e1ecfd557c64df48662791be8401a8dc1ccccf9
Souvick Mukherjee <souvick.mukherjee871@gmail.com>
2026-06-13 01:31:33 +0530

    initial commit

---
+++ backend/src/main/java/com/knowledgemesh/KnowledgemeshApplication.java
@@ -0,0 +1,13 @@
+package com.knowledgemesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgemeshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgemeshApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/knowledgemesh/KnowledgemeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knowledgemesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgemeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
